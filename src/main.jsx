2db94c7d843b69ddf624c7f2471020440ec047b0
Fondocasahub <nnigido@gmail.com>
2026-07-21 12:05:58 +0200

    Remove React.StrictMode wrapper from render

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>,
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
 )
